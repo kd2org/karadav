d9986c54340c2a52142b7aaec4ae8cb0cb21a118
bohwaz <github.bohwaz@miam.kd2.org>
2022-10-10 14:01:20 +0200

    Move JS client to a separate repo

--- Makefile
+++ Makefile
@@ -1,4 +1,8 @@
-deps:
+js-deps:
+	wget -O www/webdav.js https://raw.githubusercontent.com/kd2org/webdav-manager.js/main/webdav.js
+	wget -O www/webdav.css https://raw.githubusercontent.com/kd2org/webdav-manager.js/main/webdav.css
+
+php-deps:
 	wget -O lib/KD2/WebDAV.php 'https://fossil.kd2.org/kd2fw/doc/tip/src/lib/KD2/WebDAV.php'
 	wget -O lib/KD2/WebDAV_NextCloud.php 'https://fossil.kd2.org/kd2fw/doc/tip/src/lib/KD2/WebDAV_NextCloud.php'
 
--- lib/KaraDAV/WebDAV.php
+++ lib/KaraDAV/WebDAV.php
@@ -6,15 +6,24 @@ use KD2\WebDAV\Server as WebDAV_Server;
 
 class WebDAV extends WebDAV_Server
 {
-	protected function html_directory(string $uri, iterable $list, array $strings = self::LANGUAGE_STRINGS): ?string
+	protected function html_directory(string $uri, iterable $list): ?string
 	{
-		$out = parent::html_directory($uri, $list, $strings);
+		$out = parent::html_directory($uri, $list);
 
 		if (null !== $out) {
-			$out = str_replace('</head>', sprintf('<link rel="stylesheet" type="text/css" href="%sfiles.css" /></head>', WWW_URL), $out);
-			$out = str_replace('</body>', sprintf('<script type="text/javascript" src="%sfiles.js"></script></body>', WWW_URL), $out);
+			$out = str_replace('<body>', sprintf('<body style="opacity: 0"><script type="text/javascript" src="%swebdav.js"></script>', WWW_URL), $out);
 		}
 
 		return $out;
+	}
+
+	public function http_options(): void
+	{
+		parent::http_options();
+
+		header('Access-Control-Allow-Origin: *');
+		header('Access-Control-Allow-Credentials: true');
+		header('Access-Control-Allow-Headers: Authorization, *');
+		header('Access-Control-Allow-Methods: GET,HEAD,PUT,DELETE,COPY,MOVE,PROPFIND,MKCOL,LOCK,UNLOCK');
 	}
 }
