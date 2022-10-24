b4ef95bb6f15da109e315f164c430b955ec90edb
bohwaz <github.bohwaz@miam.kd2.org>
2022-10-24 22:35:33 +0000

    Only log when enabled
    FossilOrigin-Name: ca97291fdb486934c1a553a8cb6991eaf8991379a327267d32512a91d8cbef46

--- www/_router.php
+++ www/_router.php
@@ -9,29 +9,34 @@ $uri = strtok($_SERVER['REQUEST_URI'], '?');
 $s = new Server;
 
 $method = $_SERVER['REQUEST_METHOD'] ?? $_SERVER['REDIRECT_REQUEST_METHOD'];
-$qs = $_SERVER['QUERY_STRING'] ?? null;
-$headers = apache_request_headers();
 
-http_log("===== ROUTER: Got new request: %s from %s =====", date('d/m/Y H:i:s'), $_SERVER['REMOTE_ADDR']);
+if (LOG_FILE) {
+	$qs = $_SERVER['QUERY_STRING'] ?? null;
+	$headers = apache_request_headers();
 
-http_log("ROUTER: <= %s %s (User: %s)\nRequest headers:\n  %s",
-	$method,
-	$uri . ($qs ? '?' : '') . $qs,
-	$_SERVER['PHP_AUTH_USER'] ?? 'none',
-	implode("\n  ", array_map(fn ($v, $k) => $k . ': ' . $v, $headers, array_keys($headers)))
-);
+	http_log("===== ROUTER: Got new request: %s from %s =====", date('d/m/Y H:i:s'), $_SERVER['REMOTE_ADDR']);
+
+	http_log("ROUTER: <= %s %s (User: %s)\nRequest headers:\n  %s",
+		$method,
+		$uri . ($qs ? '?' : '') . $qs,
+		$_SERVER['PHP_AUTH_USER'] ?? 'none',
+		implode("\n  ", array_map(fn ($v, $k) => $k . ': ' . $v, $headers, array_keys($headers)))
+	);
+
+	if ($method != 'GET' && $method != 'OPTIONS' && $method != 'HEAD') {
+		http_log("ROUTER: <= Request body:\n%s", file_get_contents('php://input'));
+	}
+}
 
 if (PHP_SAPI == 'cli-server') {
+	file_put_contents('php://stderr', $uri . "\n");
+
 	if (is_file(__DIR__ . '/' . $uri)) {
 		return false;
 	}
 	// Index.php
 	elseif ($uri == '/' && $method != 'OPTIONS') {
 		return false;
-	}
-
-	if ($method == 'PROPPATCH' || $method == 'PROPFIND') {
-		http_log("ROUTER: <= Request body:\n%s", file_get_contents('php://input'));
 	}
 }
 
@@ -47,6 +52,6 @@ if (!$s->route($uri)) {
 	http_response_code(404);
 	echo '<h1>Invalid URL</h1>';
 }
-else {
+elseif (LOG_FILE) {
 	http_log("ROUTER: => %d\nResponse headers:\n  %s", http_response_code(), implode("\n  ", headers_list()));
 }
