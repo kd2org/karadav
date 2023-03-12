9b12f3a46c97f2c47fca4183426be1c94bd6a064
bohwaz <github.bohwaz@miam.kd2.org>
2023-03-12 19:40:54 +0000

    Allow to install in a subdirectory
    FossilOrigin-Name: 5bbca85d1a647ae3cee505cb243fc1aa4b2f65f56cbec284ee7759539b1838cc

--- www/_inc.php
+++ www/_inc.php
@@ -116,7 +116,7 @@ function html_head(string $title): void
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0, target-densitydpi=device-dpi" />
 		<title>{$title}</title>
-		<link rel="stylesheet" type="text/css" href="/ui.css" />
+		<link rel="stylesheet" type="text/css" href="ui.css" />
 	</head>
 	<body>
 	<h1>{$title}</h1>
