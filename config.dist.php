85d5a1482d51c62ea84349ed7211acf3dbf7c245
OniriCorpe <oniricorpe@disroot.org>
2024-01-31 05:52:15 +0100

    fix a typo in a constant name

--- config.dist.php
+++ config.dist.php
@@ -115,7 +115,7 @@ const AUTH_CALLBACK = null;
  *
  * All users logging in will be created locally and have the default quota.
  */
-const LDAP_HOST = null;
+const LDAP_URI = null;
 //const LDAP_URI = '127.0.0.1';
 
 const LDAP_LOGIN = null;
