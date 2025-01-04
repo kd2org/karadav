b28a47dac8e46fa98e6c4188c44f3713d6844f6d
bohwaz <github.bohwaz@miam.kd2.org>
2025-01-04 01:34:48 +0000

    Fix return type
    FossilOrigin-Name: 88e7588504ad89410a83464ac549d272bdaf0ec88e6dde0a9618990144e8a904

--- lib/KaraDAV/Storage.php
+++ lib/KaraDAV/Storage.php
@@ -621,7 +621,7 @@ class Storage extends AbstractStorage implements TrashInterface
 			$this->users->current()->id, $id);
 	}
 
-	public function getFileId(int $path): ?string
+	public function getFileId(int $path): ?id
 	{
 		return DB::getInstance()->firstColumn('SELECT path FROM files WHERE user = ? AND path = ?;',
 			$this->users->current()->id, $path);
