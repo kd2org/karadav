56ac954ec3242ae2cac74c8f8e9fcd1e60bd6949
Mazin Fadl <mazen@illumed.net>
2026-08-29 23:59:20 -0400

    Remove var_dump statement in oc:id property
    get_file_property() dumped and exited whenever a file or directory had no
    row in the files index. A PROPFIND requesting oc:id (e.g. an allprop listing)
    on a folder containing an unindexed entry therefore returned a truncated 200
    response instead of a 207 multistatus. Return null so the property is
    reported as not found.

--- lib/KaraDAV/Storage.php
+++ lib/KaraDAV/Storage.php
@@ -356,7 +356,7 @@ class Storage extends AbstractStorage implements TrashInterface
 			case NextCloud::PROP_OC_ID:
 				$id = $this->getFileId($uri);
 				if (!$id) {
-					var_dump($id, $uri); exit;
+					return null;
 				}
 				// ID = fileid (padded with zeros to be at least 8 characters long) + instanceid
 				$id = str_pad((string)$id, 8, '0', STR_PAD_LEFT) . 'oc' . substr(sha1(WWW_URL), 0, 10);
