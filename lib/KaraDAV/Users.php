443c96cb97a3f26d596bac5a961da44a80954518
bohwaz <github.bohwaz@miam.kd2.org>
2026-08-31 00:40:28 +0000

    Fix condition
    FossilOrigin-Name: b16f7a32f3fb8bebd3a72e589e0c1491d5e73e011b8f91e717caa04771cd52bd

--- lib/KaraDAV/Users.php
+++ lib/KaraDAV/Users.php
@@ -88,7 +88,7 @@ class Users
 
 		// Old sessions (pre-refactor) stored a stdClass with stale, byte-based quota values
 		// FIXME: remove in 1.0 release
-		if ($user instanceof stdClass) {
+		if (isset($_SESSION['user']) && $_SESSION['user'] instanceof stdClass) {
 			$_SESSION['user'] = $this->getById($user->id);
 		}
 
